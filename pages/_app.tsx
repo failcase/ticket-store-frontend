9a4721ef0e469e7e3331193d80c7e0c54b122e23
Rustam Astafeev <rustam@astafeev.dev>
2025-04-27 18:41:27 +0500

    fixed loader

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,16 @@ import '@/styles/globals.css'
 import { ThemeProvider } from 'next-themes'
 import NProgress from 'nprogress'
 import 'nprogress/nprogress.css'
+import Router from 'next/router'
+import 'nprogress/nprogress.css'
 import Footer from '@/components/Footer'
 import type { AppProps } from 'next/app';
 
 NProgress.configure({ showSpinner: false, trickleSpeed: 200 })
+
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError',   () => NProgress.done())
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
